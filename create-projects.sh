99ed5c609a33340de31eef09a2b373ee9cff17d1
Michael Russo <russo.mail@gmail.com>
2021-02-18 18:25:18 -0500

    Adds script to create microservices

---
+++ create-projects.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env sh
+
+mkdir microservices
+cd microservices
+
+spring init \
+--boot-version=2.4.3 \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=product-service \
+--package-name=com.example.microservices.core.product \
+--groupId=com.example.microservices.core.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-service
+
+spring init \
+--boot-version=2.4.3 \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=review-service \
+--package-name=com.example.microservices.core.review \
+--groupId=com.example.microservices.core.review \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+review-service
+
+spring init \
+--boot-version=2.4.3 \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=recommendation-service \
+--package-name=com.example.microservices.core.recommendation \
+--groupId=com.example.microservices.core.recommendation \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+recommendation-service
+
+spring init \
+--boot-version=2.4.3 \
+--build=gradle \
+--java-version=1.8 \
+--packaging=jar \
+--name=product-composite-service \
+--package-name=com.example.microservices.composite.product \
+--groupId=com.example.microservices.composite.product \
+--dependencies=actuator,webflux \
+--version=1.0.0-SNAPSHOT \
+product-composite-service
+
+cd ..
