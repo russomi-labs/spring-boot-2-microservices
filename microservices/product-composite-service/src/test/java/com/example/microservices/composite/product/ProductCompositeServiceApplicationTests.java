178ae782f2fef91787b802f926a4683c6621787e
Michael Russo <russo.mail@gmail.com>
2021-02-22 14:12:46 -0500

    Adds persistence to core services

--- microservices/product-composite-service/src/test/java/com/example/microservices/composite/product/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/com/example/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -1,11 +1,17 @@
 package com.example.microservices.composite.product;
 
-import static java.util.Collections.singletonList;
-import static org.mockito.Mockito.when;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import com.example.api.composite.product.ProductAggregate;
+import com.example.api.composite.product.RecommendationSummary;
+import com.example.api.composite.product.ReviewSummary;
 import com.example.api.core.product.Product;
 import com.example.api.core.recommendation.Recommendation;
 import com.example.api.core.review.Review;
@@ -13,29 +19,23 @@ import com.example.microservices.composite.product.services.ProductCompositeInte
 import com.example.util.exceptions.InvalidInputException;
 import com.example.util.exceptions.NotFoundException;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.when;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static reactor.core.publisher.Mono.just;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment=RANDOM_PORT)
 public class ProductCompositeServiceApplicationTests {
-
-	private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeServiceApplicationTests.class);
 
 	private static final int PRODUCT_ID_OK = 1;
 	private static final int PRODUCT_ID_NOT_FOUND = 2;
 	private static final int PRODUCT_ID_INVALID = 3;
 
-	@Autowired
-	private WebTestClient client;
+    @Autowired
+    private WebTestClient client;
 
 	@MockBean
 	private ProductCompositeIntegration compositeIntegration;
@@ -43,19 +43,16 @@ public class ProductCompositeServiceApplicationTests {
 	@BeforeEach
 	public void setUp() {
 
-		LOG.debug("SetUp called.");
-		when(compositeIntegration.getProduct(PRODUCT_ID_OK))
-				.thenReturn(new Product(PRODUCT_ID_OK, "name", 1, "mock-address"));
-		when(compositeIntegration.getRecommendations(PRODUCT_ID_OK)).thenReturn(
-				singletonList(new Recommendation(PRODUCT_ID_OK, 1, "author", 1, "content", "mock address")));
-		when(compositeIntegration.getReviews(PRODUCT_ID_OK)).thenReturn(
-				singletonList(new Review(PRODUCT_ID_OK, 1, "author", "subject", "content", "mock address")));
+		when(compositeIntegration.getProduct(PRODUCT_ID_OK)).
+			thenReturn(new Product(PRODUCT_ID_OK, "name", 1, "mock-address"));
+		when(compositeIntegration.getRecommendations(PRODUCT_ID_OK)).
+			thenReturn(singletonList(new Recommendation(PRODUCT_ID_OK, 1, "author", 1, "content", "mock address")));
+		when(compositeIntegration.getReviews(PRODUCT_ID_OK)).
+			thenReturn(singletonList(new Review(PRODUCT_ID_OK, 1, "author", "subject", "content", "mock address")));
 
-		when(compositeIntegration.getProduct(PRODUCT_ID_NOT_FOUND))
-				.thenThrow(new NotFoundException("NOT FOUND: " + PRODUCT_ID_NOT_FOUND));
+		when(compositeIntegration.getProduct(PRODUCT_ID_NOT_FOUND)).thenThrow(new NotFoundException("NOT FOUND: " + PRODUCT_ID_NOT_FOUND));
 
-		when(compositeIntegration.getProduct(PRODUCT_ID_INVALID))
-				.thenThrow(new InvalidInputException("INVALID: " + PRODUCT_ID_INVALID));
+		when(compositeIntegration.getProduct(PRODUCT_ID_INVALID)).thenThrow(new InvalidInputException("INVALID: " + PRODUCT_ID_INVALID));
 	}
 
 	@Test
@@ -63,29 +60,81 @@ public class ProductCompositeServiceApplicationTests {
 	}
 
 	@Test
+	public void createCompositeProduct1() {
+
+		ProductAggregate compositeProduct = new ProductAggregate(1, "name", 1, null, null, null);
+
+		postAndVerifyProduct(compositeProduct, OK);
+	}
+
+	@Test
+	public void createCompositeProduct2() {
+		ProductAggregate compositeProduct = new ProductAggregate(1, "name", 1,
+				singletonList(new RecommendationSummary(1, "a", 1, "c")),
+				singletonList(new ReviewSummary(1, "a", "s", "c")), null);
+
+		postAndVerifyProduct(compositeProduct, OK);
+	}
+
+	@Test
+	public void deleteCompositeProduct() {
+		ProductAggregate compositeProduct = new ProductAggregate(1, "name", 1,
+				singletonList(new RecommendationSummary(1, "a", 1, "c")),
+				singletonList(new ReviewSummary(1, "a", "s", "c")), null);
+
+		postAndVerifyProduct(compositeProduct, OK);
+
+		deleteAndVerifyProduct(compositeProduct.getProductId(), OK);
+		deleteAndVerifyProduct(compositeProduct.getProductId(), OK);
+	}
+
+	@Test
 	public void getProductById() {
 
-		client.get().uri("/product-composite/" + PRODUCT_ID_OK).accept(APPLICATION_JSON).exchange().expectStatus()
-				.isOk().expectHeader().contentType(APPLICATION_JSON).expectBody().jsonPath("$.productId")
-				.isEqualTo(PRODUCT_ID_OK).jsonPath("$.recommendations.length()").isEqualTo(1)
-				.jsonPath("$.reviews.length()").isEqualTo(1);
+		getAndVerifyProduct(PRODUCT_ID_OK, OK)
+            .jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
+            .jsonPath("$.recommendations.length()").isEqualTo(1)
+            .jsonPath("$.reviews.length()").isEqualTo(1);
 	}
 
 	@Test
 	public void getProductNotFound() {
 
-		client.get().uri("/product-composite/" + PRODUCT_ID_NOT_FOUND).accept(APPLICATION_JSON).exchange()
-				.expectStatus().isNotFound().expectHeader().contentType(APPLICATION_JSON).expectBody()
-				.jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND).jsonPath("$.message")
-				.isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
+		getAndVerifyProduct(PRODUCT_ID_NOT_FOUND, NOT_FOUND)
+            .jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+            .jsonPath("$.message").isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
 	}
 
 	@Test
 	public void getProductInvalidInput() {
 
-		client.get().uri("/product-composite/" + PRODUCT_ID_INVALID).accept(APPLICATION_JSON).exchange().expectStatus()
-				.isEqualTo(UNPROCESSABLE_ENTITY).expectHeader().contentType(APPLICATION_JSON).expectBody()
-				.jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_INVALID).jsonPath("$.message")
-				.isEqualTo("INVALID: " + PRODUCT_ID_INVALID);
+		getAndVerifyProduct(PRODUCT_ID_INVALID, UNPROCESSABLE_ENTITY)
+            .jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_INVALID)
+            .jsonPath("$.message").isEqualTo("INVALID: " + PRODUCT_ID_INVALID);
+	}
+
+	private WebTestClient.BodyContentSpec getAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+		return client.get()
+			.uri("/product-composite/" + productId)
+			.accept(APPLICATION_JSON)
+			.exchange()
+			.expectStatus().isEqualTo(expectedStatus)
+			.expectHeader().contentType(APPLICATION_JSON)
+			.expectBody();
+	}
+
+	private void postAndVerifyProduct(ProductAggregate compositeProduct, HttpStatus expectedStatus) {
+		client.post()
+			.uri("/product-composite")
+			.body(just(compositeProduct), ProductAggregate.class)
+			.exchange()
+			.expectStatus().isEqualTo(expectedStatus);
+	}
+
+	private void deleteAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+		client.delete()
+			.uri("/product-composite/" + productId)
+			.exchange()
+			.expectStatus().isEqualTo(expectedStatus);
 	}
 }
---
+++ microservices/recommendation-service/src/main/java/com/example/microservices/core/recommendation/services/RecommendationMapper.java
@@ -0,0 +1,29 @@
+package com.example.microservices.core.recommendation.services;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import com.example.api.core.recommendation.Recommendation;
+import com.example.microservices.core.recommendation.persistence.RecommendationEntity;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RecommendationMapper {
+
+    @Mappings({
+        @Mapping(target = "rate", source="entity.rating"),
+        @Mapping(target = "serviceAddress", ignore = true)
+    })
+    Recommendation entityToApi(RecommendationEntity entity);
+
+    @Mappings({
+        @Mapping(target = "rating", source="api.rate"),
+        @Mapping(target = "id", ignore = true),
+        @Mapping(target = "version", ignore = true)
+    })
+    RecommendationEntity apiToEntity(Recommendation api);
+
+    List<Recommendation> entityListToApiList(List<RecommendationEntity> entity);
+    List<RecommendationEntity> apiListToEntityList(List<Recommendation> api);
+}
